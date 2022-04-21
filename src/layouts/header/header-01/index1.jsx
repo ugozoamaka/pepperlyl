1918ca09b4de472fdbd57bdf0acf77cb5ca5bd97
Ugochukwu Ozoamaka <uozoamaka@gmail.com>
2022-04-21 08:09:38 +0100

    initial

--- src/layouts/header/header-01/index1.jsx
+++ src/layouts/header/header-01/index1.jsx
@@ -49,8 +49,7 @@ const Header = ({ className }) => {
                                 </div>
                                 <FlyoutSearchForm isOpen={search} />
                             </div>
-                        </div>
-                        <div className="header-right">
+
                             <div className="mainmenu-wrapper">
                                 <nav
                                     id="sideNav"
@@ -59,6 +58,8 @@ const Header = ({ className }) => {
                                     <MainMenu menu={menuData} />
                                 </nav>
                             </div>
+                        </div>
+                        <div className="header-right">
                             {!isAuthenticated && (
                                 <div className="setting-option header-btn">
                                     <div className="icon-box">
@@ -78,25 +79,25 @@ const Header = ({ className }) => {
                                     <UserDropdown />
                                 </div>
                             )}
-                            {/* <div className="setting-option rn-icon-list notification-badge">
+                            <div className="setting-option rn-icon-list notification-badge">
                                 <div className="icon-box">
                                     <Anchor path={headerData.activity_link}>
                                         <i className="feather-bell" />
                                         <span className="badge">6</span>
                                     </Anchor>
                                 </div>
-                            </div> */}
-                            {/* <div className="setting-option mobile-menu-bar d-block d-xl-none">
+                            </div>
+                            <div className="setting-option mobile-menu-bar d-block d-xl-none">
                                 <div className="hamberger">
                                     <BurgerButton onClick={offcanvasHandler} />
                                 </div>
-                            </div> */}
-                            {/* <div
+                            </div>
+                            <div
                                 id="my_switcher"
                                 className="setting-option my_switcher"
                             >
                                 <ColorSwitcher />
-                            </div> */}
+                            </div>
                         </div>
                     </div>
                 </div>
